fed2eb6ebdd626ba474b024457cd0cc421972741
Jaime Valencia <javalenciab90@gmail.com>
2023-05-21 08:48:11 -0500

    last detail set

--- app/src/main/java/com/example/instalogin/ui/login/BodyLogin.kt
+++ app/src/main/java/com/example/instalogin/ui/login/BodyLogin.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
@@ -154,7 +156,7 @@ fun LoginButton(loginEnable: Boolean) {
         ),
         modifier = Modifier.fillMaxWidth(),
     ) {
-        Text(text = "Log In")
+        Text(text = "Log In", modifier = Modifier.padding(4.dp))
     }
 }
 

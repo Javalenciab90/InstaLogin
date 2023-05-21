185ff1c77a4b81fddc69778c9c666a7b6d39a211
Jaime Valencia <javalenciab90@gmail.com>
2023-05-21 07:21:05 -0500

    Customized design of Composables

--- app/src/main/java/com/example/instalogin/ui/LoginScreen.kt
+++ app/src/main/java/com/example/instalogin/ui/LoginScreen.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,16 +14,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -34,6 +40,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.instalogin.R
@@ -158,7 +167,17 @@ fun LoginDivider() {
 
 @Composable
 fun LoginButton(loginEnable: Boolean) {
-    Button(onClick = {  }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+    Button(
+        onClick = {  },
+        enabled = loginEnable,
+        colors = ButtonDefaults.buttonColors(
+          backgroundColor = Color(0xFF4EA8E9),
+          disabledBackgroundColor = Color(0xFF78C8F9),
+          contentColor = Color.White,
+          disabledContentColor = Color.White
+        ),
+        modifier = Modifier.fillMaxWidth(),
+    ) {
         Text(text = "Log In")
     }
 }
@@ -177,9 +196,28 @@ fun ForgotPassWordTextView(modifier: Modifier) {
 
 @Composable
 fun PasswordTextField(password: String, onTextChanged: (String) -> Unit) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
     TextField(
         value = password,
         onValueChange = { text -> onTextChanged(text) },
+        placeholder = { Text(text = "Password") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            backgroundColor = Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            unfocusedLabelColor = Color.Transparent
+        ),
+        trailingIcon = {
+            val image =
+                if (isPasswordVisible) Icons.Filled.VisibilityOff else Icons.Filled.Visibility
+            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                Icon(imageVector = image, contentDescription = "show password")
+            }
+        },
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
         modifier = Modifier.fillMaxWidth()
     )
 }
@@ -189,7 +227,18 @@ fun EmailTextField(email: String, onTextChanged: (String) -> Unit) {
     TextField(
         value = email,
         onValueChange = { text -> onTextChanged(text) },
-        modifier = Modifier.fillMaxWidth()
+        placeholder = { Text(text = "Email") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            backgroundColor = Color(0xFFFAFAFA),
+            unfocusedIndicatorColor = Color.Transparent,
+            unfocusedLabelColor = Color.Transparent
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
     )
 }
 
